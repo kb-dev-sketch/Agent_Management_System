890c2f9b44564be5475f7a34658ccd511acd4013
kb-dev-sketch <kb61406@gmail.com>
2026-06-03 15:30:41 +0530

    schema of task

---
+++ src/models/task-model.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+const taskSchema=new mongoose.Schema({
+    firstName:{
+        type:String,
+        required:true
+    },
+    phone:{
+        type:String,
+        required:true
+    },
+    notes:{
+        type:String,
+        required:true
+    },
+    assignedAgent:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Agent"
+    }
+
+});
+export default mongoose.model("Task", taskSchema);
