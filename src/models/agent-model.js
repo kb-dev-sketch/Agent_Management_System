0eed2604d026c30b8f78e21a34b9a1e7b5a79538
kb-dev-sketch <kb61406@gmail.com>
2026-06-03 15:28:14 +0530

    agent schema

---
+++ src/models/agent-model.js
@@ -0,0 +1,23 @@
+import mongoose from"mpngoose ";
+const agentSchema=new mongoose.Schema({
+firstName:{
+    type:String,
+    required:true,
+},
+email:{
+    type:String,
+    required:true,
+    unique:true,
+},
+password:{
+    type:String,
+    required:true,
+},
+phoneNumber:{
+    type:String,
+    required:true,
+    unique:true,
+}
+
+    })
+    module.exports=mongoose.model("Agent",agentSchema)
