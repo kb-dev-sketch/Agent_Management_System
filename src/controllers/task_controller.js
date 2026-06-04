76e335d1c93ba0d463b930ed2c2c3690afe63c85
kb-dev-sketch <kb61406@gmail.com>
2026-06-04 21:57:33 +0530

    full map of project

--- src/controllers/task_controller.js
+++ src/controllers/task_controller.js
@@ -1,4 +1,5 @@
-const Task = require("../models/task_model");
+const Task = require("../models/task-model");
+const Agent = require("../models/agent-model");
 
 const getTasks = async (req, res) => {
   try {
@@ -16,6 +17,50 @@ const getTasks = async (req, res) => {
   }
 };
 
+const assignAgentsToTasks = async (req, res) => {
+  try {
+    const agents = await Agent.find();
+    if (!agents.length) {
+      return res.status(400).json({
+        message: "Add at least one agent first",
+      });
+    }
+
+    const agentIds = agents.map((a) => a._id);
+
+    const unassigned = await Task.find({
+      $or: [
+        { assignedAgent: null },
+        { assignedAgent: { $exists: false } },
+        { assignedAgent: { $nin: agentIds } },
+      ],
+    }).sort({ _id: 1 });
+
+    if (!unassigned.length) {
+      return res.status(200).json({
+        message: "All tasks already have an agent",
+        updated: 0,
+      });
+    }
+
+    for (let i = 0; i < unassigned.length; i++) {
+      const agent = agents[i % agents.length];
+      unassigned[i].assignedAgent = agent._id;
+      await unassigned[i].save();
+    }
+
+    return res.status(200).json({
+      message: "Agents assigned to tasks",
+      updated: unassigned.length,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
 module.exports = {
   getTasks,
+  assignAgentsToTasks,
 };
--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const {
   getTasks,
+  assignAgentsToTasks,
 } = require("../controllers/task_controller");
 
 router.get("/", getTasks);
+router.post("/assign-agents", assignAgentsToTasks);
 
 module.exports = router;
